273626acad05e9df46851174d90c6f5073b91341
AlbedoUniversal <Albedogforce@gmail.com>
2020-01-09 17:29:54 +0300

    nn

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,13 @@ $(function() {
     slidesToScroll: 4,
     asNavFor: ".header__slider"
   });
+
+  $(".serf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="" />'
+  });
 });
